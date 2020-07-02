f194ca9081d5d8158c7721d6b7a54436410af7af
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-07-01 21:05:10 -0400

    html routes added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //dependencies
 // ------------------------------
 const express = require("express");
+var path = require("path");
 const fs = require("fs");
 
 //setting up express
@@ -8,10 +9,25 @@ const fs = require("fs");
 const app = express();
 const PORT = 3000;
 
-//server is listening
+// seting up express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routes HTML
 //-------------------------------
-app.listen(PORT, ()=>{
-    console.log(`App listening on  PORT: http://localhost:${PORT}`)
+
+//route that sends user to homepage
+app.get("*", (res, req) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
+//route that sends user to notes page
+app.get("/notes", (res, req) => {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
 
+//server is listening
+//-------------------------------
+app.listen(PORT, () => {
+  console.log(`App listening on  PORT: http://localhost:${PORT}`);
+});
